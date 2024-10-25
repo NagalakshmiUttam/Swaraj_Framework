e00275dd6a967b0ff30fb5049e7665386699ba07
Nagalakshmi <nagalakshmi.90246@gmail.com>
2024-10-25 15:50:55 +0530

    Push all folders

--- Swaraj_lease_b2b/src/test/java/com/swaraj/testcases/script_without_FWS.java
+++ Swaraj_lease_b2b/src/test/java/com/swaraj/testcases/script_without_FWS.java
@@ -27,11 +27,25 @@ public static void main(String[] args) throws InterruptedException {
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("newuser@gmail.com");
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("newuser@gmail.com@1");
-		Thread.sleep(8000);
+		Thread.sleep(5000);
 		driver.findElement(By.xpath("//button[@class='btn-primary_1 btn-login flex-center ']")).click();
-
+		Thread.sleep(6000);
+//		driver.findElement(By.xpath("//div[@data-title='Notification']")).click();
+//		driver.findElement(By.xpath("//div[@data-title='Announcement']")).click();
+		Thread.sleep(6000);
+		driver.findElement(By.xpath("(//a[@class='topBarHead'])[1]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//button[normalize-space()='Add Property']//*[name()='svg']")).click();
+		driver.findElement(By.xpath("//input[@id='propertyName']")).sendKeys("prop1");
+		driver.findElement(By.xpath("//textarea[@id='propertyAddress']")).sendKeys("prop1");
+		driver.findElement(By.xpath("//input[@id='unitName']")).sendKeys("unit1");
+		driver.findElement(By.xpath("//textarea[@id='unitDesc']")).sendKeys("two BHK");
+		driver.findElement(By.xpath("//input[@id='unitRentPerMonth']")).sendKeys("5000");
+		driver.findElement(By.xpath("//input[@id='unitInc']")).sendKeys("10");
+		Thread.sleep(5000);
+		
 		//Close the browser
-//		driver.quit();
+		driver.quit();
 
 	}
 
